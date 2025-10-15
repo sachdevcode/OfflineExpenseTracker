44650eb7ea8cc01b786ae89a66abbdc890295a74
sachdevcode <karancode.work@gmail.com>
2025-10-15 23:48:27 +0500

    refactor(config): add tsconfig path aliases and project folder structure setup

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,20 @@ module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
     'react-native-worklets/plugin',
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@app': './src/app',
+          '@features': './src/features',
+          '@shared': './src/shared',
+          '@navigation': './src/navigation',
+          '@config': './src/config',
+          '@store': './src/store',
+          '@assets': './src/shared/assets',
+          '@types': './src/types',
+        },
+      },
+    ],
   ],
 };
